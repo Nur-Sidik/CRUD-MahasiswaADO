3ab2d22461b497365d28e712f7db8ee41be31fa0
Nur-Sidik <nursidik1500@gmail.com>
2026-06-21 20:26:24 +0700

    Membuat UI dan logic Form 2 untuk Input Data

---
+++ CRUDMahasiswaADO/Form2.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace CRUDMahasiswaADO
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblProdi = new System.Windows.Forms.Label();
+            this.lblTahun = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dTP = new System.Windows.Forms.DateTimePicker();
+            this.BtnReport = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dBAkademikADODataSet = new CRUDMahasiswaADO.DBAkademikADODataSet();
+            this.mahasiswaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.mahasiswaTableAdapter = new CRUDMahasiswaADO.DBAkademikADODataSetTableAdapters.MahasiswaTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCetak = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dBAkademikADODataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mahasiswaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblProdi
+            // 
+            this.lblProdi.AutoSize = true;
+            this.lblProdi.Location = new System.Drawing.Point(21, 66);
+            this.lblProdi.Name = "lblProdi";
+            this.lblProdi.Size = new System.Drawing.Size(31, 13);
+            this.lblProdi.TabIndex = 0;
+            this.lblProdi.Text = "Prodi";
+            // 
+            // lblTahun
+            // 
+            this.lblTahun.AutoSize = true;
+            this.lblTahun.Location = new System.Drawing.Point(329, 66);
+            this.lblTahun.Name = "lblTahun";
+            this.lblTahun.Size = new System.Drawing.Size(73, 13);
+            this.lblTahun.TabIndex = 1;
+            this.lblTahun.Text = "Tahun Masuk";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(80, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(226, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // dTP
+            // 
+            this.dTP.Location = new System.Drawing.Point(419, 64);
+            this.dTP.MaxDate = new System.DateTime(2030, 12, 31, 0, 0, 0, 0);
+            this.dTP.MinDate = new System.DateTime(2026, 6, 12, 0, 0, 0, 0);
+            this.dTP.Name = "dTP";
+            this.dTP.Size = new System.Drawing.Size(186, 20);
+            this.dTP.TabIndex = 3;
+            this.dTP.Value = new System.DateTime(2026, 6, 12, 11, 1, 42, 0);
+            // 
+            // BtnReport
+            // 
+            this.BtnReport.Location = new System.Drawing.Point(658, 59);
+            this.BtnReport.Name = "BtnReport";
+            this.BtnReport.Size = new System.Drawing.Size(91, 26);
+            this.BtnReport.TabIndex = 4;
+            this.BtnReport.Text = "Load";
+            this.BtnReport.UseVisualStyleBackColor = true;
+            this.BtnReport.Click += new System.EventHandler(this.BtnReport_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(329, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rekap Data";
+            // 
+            // dBAkademikADODataSet
+            // 
+            this.dBAkademikADODataSet.DataSetName = "DBAkademikADODataSet";
+            this.dBAkademikADODataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // mahasiswaBindingSource
+            // 
+            this.mahasiswaBindingSource.DataMember = "Mahasiswa";
+            this.mahasiswaBindingSource.DataSource = this.dBAkademikADODataSet;
+            // 
+            // mahasiswaTableAdapter
+            // 
+            this.mahasiswaTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(737, 308);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // btnCetak
+            // 
+            this.btnCetak.Enabled = false;
+            this.btnCetak.Location = new System.Drawing.Point(658, 419);
+            this.btnCetak.Name = "btnCetak";
+            this.btnCetak.Size = new System.Drawing.Size(94, 23);
+            this.btnCetak.TabIndex = 7;
+            this.btnCetak.Text = "Cetak Laporan";
+            this.btnCetak.UseVisualStyleBackColor = true;
+            this.btnCetak.Click += new System.EventHandler(this.btnCetak_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCetak);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BtnReport);
+            this.Controls.Add(this.dTP);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblTahun);
+            this.Controls.Add(this.lblProdi);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dBAkademikADODataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mahasiswaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProdi;
+        private System.Windows.Forms.Label lblTahun;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dTP;
+        private System.Windows.Forms.Button BtnReport;
+        private System.Windows.Forms.Label label3;
+        private DBAkademikADODataSet dBAkademikADODataSet;
+        private System.Windows.Forms.BindingSource mahasiswaBindingSource;
+        private DBAkademikADODataSetTableAdapters.MahasiswaTableAdapter mahasiswaTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCetak;
+    }
+}
---
+++ CRUDMahasiswaADO/Form2.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form2 : Form
+    {
+        private readonly SqlConnection conn;
+        private readonly string connectionString = "Data Source=MSI\\UNKNOWNMEMBER;Initial Catalog=DBAkademikADO; Integrated Security=True";
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DataTable dtProdi;
+
+        public Form2()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            dTP.Format = DateTimePickerFormat.Custom;
+            dTP.CustomFormat = "yyyy";
+            dTP.ShowUpDown = true;
+            dTP.MinDate = new DateTime(2000, 1, 1);
+            dTP.MaxDate = DateTime.Now;
+
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            btnCetak.Enabled = false;
+
+            try
+            { 
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("select namaprodi from programstudi", conn);
+                cmd.CommandType = CommandType.Text;
+                dtProdi = new DataTable();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(dtProdi);
+                comboBox1.DataSource = dtProdi;
+                comboBox1.DisplayMember = "namaprodi";
+                comboBox1.ValueMember = "namaprodi";
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data: " + ex.Message);
+            }
+
+            // TODO: This line of code loads data into the 'dBAkademikADODataSet.Mahasiswa' table. You can move, or remove it, as needed.
+            this.mahasiswaTableAdapter.Fill(this.dBAkademikADODataSet.Mahasiswa);
+
+        }
+
+        private void BtnReport_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("sp_Report", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@inProdi", SqlDbType.VarChar, 50).Value = comboBox1.Text.ToString();
+                cmd.Parameters.Add("@inTglMsuk", SqlDbType.VarChar, 4).Value = dTP.Value.Year.ToString(); ;
+
+                da = new SqlDataAdapter(cmd);
+
+                dtMahasiswa = new DataTable();
+                da.Fill(dtMahasiswa);
+
+                dataGridView1.DataSource = dtMahasiswa;
+
+                if (dtMahasiswa.Rows.Count > 0)
+                {
+
+                    BtnReport.Enabled = true;
+                }
+                else
+                {
+
+                    BtnReport.Enabled = false;
+                    MessageBox.Show("Data tidak ditemukan");
+                }
+            }
+            catch (Exception ex)
+
+            {
+                MessageBox.Show("Gagal load data: " + ex.Message);
+
+            }
+            
+        }
+
+        private void btnCetak_Click(object sender, EventArgs e)
+        {
+            Form3 frm3 = new Form3(comboBox1.SelectedValue.ToString(), dTP.Value);
+            frm3.Show();
+            this.Hide();
+        }
+    }
+}
