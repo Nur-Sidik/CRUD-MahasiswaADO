c8525fea08dc2db8004da3446466b5956304813d
Nur-Sidik <nursidik1500@gmail.com>
2026-06-21 20:26:47 +0700

    Membuat UI dan logic Form 3 untuk Edit dan Delete

---
+++ CRUDMahasiswaADO/Form3.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form3 : Form
+    {
+        private readonly SqlConnection conn;
+        private readonly string connectionString = "Data Source=MSI\\UNKNOWNMEMBER;Initial Catalog=DBAkademikADO; Integrated Security=True";
+        ReportMahasiswa rptMhs = new ReportMahasiswa();
+        private string prodi { get; set; }
+        private DateTime tglmasuk { get; set; }
+        public Form3(string Prodi, DateTime TglMasuk)
+        {
+            InitializeComponent();
+
+            prodi = Prodi;
+            tglmasuk = TglMasuk;
+
+            conn = new SqlConnection(connectionString);
+
+            LoadCrystalReport();
+        }
+
+        private void LoadCrystalReport()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                SqlCommand cmd = new SqlCommand("sp_Report", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@inProdi", prodi);
+                cmd.Parameters.AddWithValue("@inTglMsuk", tglmasuk.Year);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dtMahasiswa = new DataTable();
+                da.Fill(dtMahasiswa);
+
+                conn.Close();
+
+                rptMhs.SetDataSource(dtMahasiswa);
+
+                crystalReportViewer1.ReportSource = rptMhs;
+                crystalReportViewer1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menampilkan report: " + ex.Message);
+            }
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
